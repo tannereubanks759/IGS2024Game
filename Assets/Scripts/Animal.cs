7a630fe27dc81ecacdc9880ae421158743b1c855
tannereubanks759 <112025050+tannereubanks759@users.noreply.github.com>
2024-01-18 16:20:43 -0500

    tanner commit

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal
+{
+    public bool isDead;
+    public float walkSpeed;
+    public float runSpeed;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+}
