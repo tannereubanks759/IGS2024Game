7bceee0dc782fafac6994b7f9dbdf6e5c213cc04
jrpsus <jpaesano@ltu.edu>
2024-01-18 16:22:49 -0500

    start screen

---
+++ Assets/Scripts/StartScreenMain.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMain : MonoBehaviour
+{
+    int menuPage = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
