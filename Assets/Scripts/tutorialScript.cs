82a00243516e3a91c9290696c1f9cd380f13aa2f
Andrewk57 <112024432+Andrewk57@users.noreply.github.com>
2024-04-09 16:38:00 -0400

    tutorial

---
+++ Assets/Scripts/tutorialScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialScript : MonoBehaviour
+{
+    public GameObject[] popups;
+    private int popUpIndex;
+    private void Update()
+    {
+        for (int i = 0; i < popups.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popups[i].SetActive(true);
+            }
+            else
+            {
+                popups[i].SetActive(false);
+            }
+        }
+        if (popUpIndex == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 1)
+        {
+           
+        }
+        else if (popUpIndex == 2)
+        {
+           
+        }
+
+    }
+}
