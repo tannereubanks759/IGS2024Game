d9a63cc43099fbc7d942259dddccf2d471fff951
CJSmith325 <csmith14@ltu.edu>
2024-04-16 16:37:09 -0400

    added fire turn in sound and animation
    added fire script, fire animation, and fire sound to turn in script

---
+++ Assets/fire.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fire : MonoBehaviour
+{
+    public void DisableFire()
+    {
+        Destroy(this.gameObject);
+    }
+}
